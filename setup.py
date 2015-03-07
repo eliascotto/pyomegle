a5d09205d0270eb6607ab734eb89a48369d5c8fd
elias94 <elia.scotto@vallauri.edu>
2015-03-07 22:44:39 +0100

    Fixed setup.py url

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyomegle',
+      version='1.0',
+      description='Python API for Omegle webchat',
+      url='https://github.com/elias94/pyomegle',
+      author='Elia Scotto',
+      author_email='frodohack@gmail.com',
+      license='MIT',
+      packages=['pyomegle'],
+      zip_safe=False)
