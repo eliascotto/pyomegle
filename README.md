9cd5c361a5e2e87d34c2040f9b710024c02747d7
elias94 <elia.scotto@vallauri.edu>
2015-03-07 23:17:15 +0100

    Update setup

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# pyomegle
-Python API for Omegle webchat
+# Pyomegle
+Python API for Omegle webchat.
+Inspired by [brianewing](https://github.com/brianewing/pyomegle)
+
+# Usage
+``` python
+from pyomegle import OmegleBot, OmegleHandler
+
+"""
+    Omegle inteface for python
+
+    /next
+        starts a new conversation
+    /exit
+        exits chat session
+"""
+
+h = OmegleHandler(loop=True)
+c = OmegleBot(h, wpm=47, lang='it')
+c.start()
+
+while 1:
+    input_str = raw_input('') #string input
+
+    if input_str.strip() == '/next':
+        c.next()
+    elif input_str.strip() == '/exit':
+        break
+    else:
+        c.send(input_str)
+```
--- setup.py
+++ setup.py
@@ -1,11 +1,27 @@
-from setuptools import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(name='pyomegle',
       version='1.0',
       description='Python API for Omegle webchat',
-      url='https://github.com/elias94/pyomegle',
+      long_description='Python API for Omegle webchat. \
+                        Usage: https://github.com/elias94/pyomegle',
       author='Elia Scotto',
       author_email='frodohack@gmail.com',
+      url='https://github.com/elias94/pyomegle',
       license='MIT',
-      packages=['pyomegle'],
+      packages=[ 'pyomegle' ],
+      install_requires=[ 'mechanize' ],
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Topic :: Communications :: Chat',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP :: Browsers'
+      ],
       zip_safe=False)
